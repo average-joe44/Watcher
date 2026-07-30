49140087ddd8ed97da00599437064910854b23c1
average-joe44 <shan16042011@gmail.com>
2026-07-30 17:13:05 +0700

    Refactor socket parameter and fix download completion
    Refactor socket parameter names for clarity and fix print statement for downloaded file completion.

--- fileattacker.py
+++ fileattacker.py
@@ -1,41 +1,41 @@
 import struct
 import os
 
-def recv_all(sok, n):
+def recv_all(sock, n):
     data = bytearray()
     while len(data) < n:
-        packet = sok.recv(n - len(data))
+        packet = sock.recv(n - len(data))
         if not packet:
             return None
         data.extend(packet)
     return bytes(data)
 
-def upload_file(_target, namafile):
+def upload_file(sock, namafile):
     bufsize = 65536
     if not os.path.exists(namafile):
-        _target.sendall(struct.pack("Q", 0))
+        sock.sendall(struct.pack("Q", 0))
         print('file not found')
         return
     if os.path.isdir(namafile):
-        _target.sendall(struct.pack("Q", 0))
+        sock.sendall(struct.pack("Q", 0))
         print(f'{namafile} is a directory')
         return
     
     filesize = os.path.getsize(namafile)
-    _target.sendall(struct.pack("Q", filesize))
+    sock.sendall(struct.pack("Q", filesize))
     with open(namafile, 'rb') as f:
         print('uploading')
         while True:
             data = f.read(bufsize)
             if not data:
                 break
-            _target.sendall(data)
+            sock.sendall(data)
             print(f'{f.tell()}/{filesize} bytes ({f.tell()/filesize*100:.2f}%)', end='\r')
         print('\nuploaded')  
 
-def download_file(_target, namafile):
+def download_file(sock, namafile):
     bufsize = 65536
-    size_data = recv_all(_target, 8)
+    size_data = recv_all(sock, 8)
     if not size_data:
         return
     
@@ -52,7 +52,7 @@ def download_file(_target, namafile):
         print('downloading')
         while recv < filesize:
                 to_read = min(bufsize, filesize - recv)
-                data = _target.recv(to_read)
+                data = sock.recv(to_read)
                 if not data:
                     break
                 file.write(data)
